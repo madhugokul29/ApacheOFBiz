216874b83435642a08e4e9931eda5b934cda8267
Nicolas Malin <nicolas.malin@nereide.fr>
2016-12-05 21:55:03 +0100

    move ftl macro to common theme

--- framework/widget/src/main/java/org/apache/ofbiz/widget/renderer/macro/MacroScreenViewHandler.java
+++ framework/widget/src/main/java/org/apache/ofbiz/widget/renderer/macro/MacroScreenViewHandler.java
@@ -55,6 +55,8 @@ import freemarker.template.utility.StandardCompress;
 public class MacroScreenViewHandler extends AbstractViewHandler {
 
     public static final String module = MacroScreenViewHandler.class.getName();
+    public static final String defaultWidgetConfigProperties = "commonWidget";
+    public String visualThemeWidgetConfigProperties = null;
 
     protected ServletContext servletContext = null;
 
@@ -64,53 +66,26 @@ public class MacroScreenViewHandler extends AbstractViewHandler {
 
     private ScreenStringRenderer loadRenderers(HttpServletRequest request, HttpServletResponse response,
             Map<String, Object> context, Writer writer) throws GeneralException, TemplateException, IOException {
-        String screenMacroLibraryPath = UtilProperties.getPropertyValue("widget", getName() + ".screenrenderer");
-        String formMacroLibraryPath = UtilProperties.getPropertyValue("widget", getName() + ".formrenderer");
-        String treeMacroLibraryPath = UtilProperties.getPropertyValue("widget", getName() + ".treerenderer");
-        String menuMacroLibraryPath = UtilProperties.getPropertyValue("widget", getName() + ".menurenderer");
-        Map<String, Object> userPreferences = UtilGenerics.cast(context.get("userPreferences"));
-        if (userPreferences != null) {
-            String visualThemeId = (String) userPreferences.get("VISUAL_THEME");
-            if (visualThemeId != null) {
-                LocalDispatcher dispatcher = (LocalDispatcher) context.get("dispatcher");
-                Map<String, Object> serviceCtx = dispatcher.getDispatchContext().makeValidContext("getVisualThemeResources",
-                        ModelService.IN_PARAM, context);
-                serviceCtx.put("visualThemeId", visualThemeId);
-                Map<String, Object> serviceResult = dispatcher.runSync("getVisualThemeResources", serviceCtx);
-                if (ServiceUtil.isSuccess(serviceResult)) {
-                    Map<String, List<String>> themeResources = UtilGenerics.cast(serviceResult.get("themeResources"));
-                    List<String> resourceList = UtilGenerics.cast(themeResources.get("VT_SCRN_MACRO_LIB"));
-                    if (resourceList != null && !resourceList.isEmpty()) {
-                        String macroLibraryPath = resourceList.get(0);
-                        if (macroLibraryPath != null) {
-                            screenMacroLibraryPath = macroLibraryPath;
-                        }
-                    }
-                    resourceList = UtilGenerics.cast(themeResources.get("VT_FORM_MACRO_LIB"));
-                    if (resourceList != null && !resourceList.isEmpty()) {
-                        String macroLibraryPath = resourceList.get(0);
-                        if (macroLibraryPath != null) {
-                            formMacroLibraryPath = macroLibraryPath;
-                        }
-                    }
-                    resourceList = UtilGenerics.cast(themeResources.get("VT_TREE_MACRO_LIB"));
-                    if (resourceList != null && !resourceList.isEmpty()) {
-                        String macroLibraryPath = resourceList.get(0);
-                        if (macroLibraryPath != null) {
-                            treeMacroLibraryPath = macroLibraryPath;
-                        }
-                    }
-                    resourceList = UtilGenerics.cast(themeResources.get("VT_MENU_MACRO_LIB"));
-                    if (resourceList != null && !resourceList.isEmpty()) {
-                        String macroLibraryPath = resourceList.get(0);
-                        if (macroLibraryPath != null) {
-                            menuMacroLibraryPath = macroLibraryPath;
-                        }
-                    }
-                }
-            }
+
+        //resolve default value for macro lib
+        String propertyNameScreenRenderer = getName() + ".screenrenderer";
+        String propertyNameFormRenderer = getName() + ".formrenderer";
+        String propertyNameTreeRenderer = getName() + ".treerenderer";
+        String propertyNameMenuRenderer = getName() + ".menurenderer";
+        String screenMacroLibraryPath = UtilProperties.getPropertyValue(defaultWidgetConfigProperties, propertyNameScreenRenderer);
+        String formMacroLibraryPath = UtilProperties.getPropertyValue(defaultWidgetConfigProperties, propertyNameFormRenderer);
+        String treeMacroLibraryPath = UtilProperties.getPropertyValue(defaultWidgetConfigProperties, propertyNameTreeRenderer);
+        String menuMacroLibraryPath = UtilProperties.getPropertyValue(defaultWidgetConfigProperties, propertyNameMenuRenderer);
+
+        //if theme have a specific property widget file, resolve the path
+        if (visualThemeWidgetConfigProperties != null) {
+            screenMacroLibraryPath = UtilProperties.getPropertyValue(visualThemeWidgetConfigProperties, propertyNameScreenRenderer, screenMacroLibraryPath);
+            formMacroLibraryPath = UtilProperties.getPropertyValue(visualThemeWidgetConfigProperties, propertyNameFormRenderer, formMacroLibraryPath);
+            treeMacroLibraryPath = UtilProperties.getPropertyValue(visualThemeWidgetConfigProperties, propertyNameTreeRenderer, treeMacroLibraryPath);
+            menuMacroLibraryPath = UtilProperties.getPropertyValue(visualThemeWidgetConfigProperties, propertyNameMenuRenderer, menuMacroLibraryPath);
         }
-        ScreenStringRenderer screenStringRenderer = new MacroScreenRenderer(UtilProperties.getPropertyValue("widget", getName()
+
+        ScreenStringRenderer screenStringRenderer = new MacroScreenRenderer(UtilProperties.getPropertyValue(defaultWidgetConfigProperties, getName()
                 + ".name"), screenMacroLibraryPath);
         if (!formMacroLibraryPath.isEmpty()) {
             FormStringRenderer formStringRenderer = new MacroFormRenderer(formMacroLibraryPath, request, response);
@@ -129,11 +104,39 @@ public class MacroScreenViewHandler extends AbstractViewHandler {
 
     public void render(String name, String page, String info, String contentType, String encoding, HttpServletRequest request, HttpServletResponse response) throws ViewHandlerException {
         try {
+            MapStack<String> context = MapStack.create();
+            ScreenRenderer.populateContextForRequest(context, null, request, response, servletContext);
+
+            //Resolve the visual theme
+            String visualThemeId = (String) context.get("visualThemeId");
+            visualThemeWidgetConfigProperties = null;
+            if (visualThemeId == null) {
+                Map<String, Object> userPreferences = UtilGenerics.cast(context.get("userPreferences"));
+                if (userPreferences != null) {
+                    visualThemeId = (String) userPreferences.get("VISUAL_THEME");
+                    if (visualThemeId != null) {
+                        LocalDispatcher dispatcher = (LocalDispatcher) context.get("dispatcher");
+                        Map<String, Object> serviceCtx = dispatcher.getDispatchContext().makeValidContext("getVisualThemeResources",
+                                ModelService.IN_PARAM, context);
+                        serviceCtx.put("visualThemeId", visualThemeId);
+                        Map<String, Object> serviceResult = dispatcher.runSync("getVisualThemeResources", serviceCtx);
+                        if (ServiceUtil.isSuccess(serviceResult)) {
+                            Map<String, List<String>> themeResources = UtilGenerics.cast(serviceResult.get("themeResources"));
+                            List<String> resourceList = UtilGenerics.cast(themeResources.get("VT_WIDGET_CONFIG"));
+                            if (resourceList != null && ! resourceList.isEmpty()) {
+                                visualThemeWidgetConfigProperties = resourceList.get(0);
+                            }
+                        }
+                    }
+                } else {
+                    visualThemeId = "COMMON";
+                }
+            }
             Writer writer = response.getWriter();
             Delegator delegator = (Delegator) request.getAttribute("delegator");
             // compress output if configured to do so
             if (UtilValidate.isEmpty(encoding)) {
-                encoding = EntityUtilProperties.getPropertyValue("widget", getName() + ".default.encoding", "none", delegator);
+                encoding = EntityUtilProperties.getPropertyValue(defaultWidgetConfigProperties, getName() + ".default.encoding", "none", delegator);
             }
             boolean compressOutput = "compressed".equals(encoding);
             if (!compressOutput) {
@@ -147,12 +150,16 @@ public class MacroScreenViewHandler extends AbstractViewHandler {
                 // to speed up output.
                 writer = new StandardCompress().getWriter(writer, null);
             }
-            MapStack<String> context = MapStack.create();
-            ScreenRenderer.populateContextForRequest(context, null, request, response, servletContext);
             ScreenStringRenderer screenStringRenderer = loadRenderers(request, response, context, writer);
             ScreenRenderer screens = new ScreenRenderer(writer, context, screenStringRenderer);
             context.put("screens", screens);
-            context.put("simpleEncoder", UtilCodec.getEncoder(UtilProperties.getPropertyValue("widget", getName() + ".encoder")));
+
+            //resolve encoder
+            String encoderName = UtilProperties.getPropertyValue(defaultWidgetConfigProperties, getName() + ".encoder");
+            if (visualThemeWidgetConfigProperties != null) {
+                encoderName = UtilProperties.getPropertyValue(visualThemeWidgetConfigProperties, getName() + ".encoder", encoderName);
+            }
+            context.put("simpleEncoder", UtilCodec.getEncoder(encoderName));
             screenStringRenderer.renderScreenBegin(writer, context);
             screens.render(page);
             screenStringRenderer.renderScreenEnd(writer, context);
